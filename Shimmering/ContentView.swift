3d46eec1790a83a8138a9c20a8e5179d5dffb03e
Justin Lee <lis186@gmail.com>
2024-07-24 23:38:15 +0800

    Initial Commit

---
+++ Shimmering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shimmering
+//
+//  Created by Yi-Hsiu Lee on 2024/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shimmering/ShimmeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmeringApp.swift
+//  Shimmering
+//
+//  Created by Yi-Hsiu Lee on 2024/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
